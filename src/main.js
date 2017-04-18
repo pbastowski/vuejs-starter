a0b34c2f59feeb747486940c03f4d6ccf2c6b870
Paul Bastowski <pbastowski@gmail.com>
2017-04-18 17:15:55 +0200

    More tweaks

--- src/main.js
+++ src/main.js
@@ -1,3 +1,6 @@
+// Uncomment the line below if you need google analytics
+// import './common/analytics'
+
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
